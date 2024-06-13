e71ab2db5ff133358a3a60e5d412a3f24bd08ea6
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-06-13 11:36:11 -0300

    consertando o menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+/*Esse script faz com que adicione 2 constantes (que uma é paara abrir o menu e a outra para fechar) e elas estao dentro de uma função. E na função do menuToggle, ela adiciona um 'listener', que ao clicar nele, se ativara o active e nele no css, faz com q o display seja flex, pois o na css .menu ele está none.   */
+
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('ham-menu');
     const menu = document.getElementById('menu');
@@ -6,3 +8,4 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('active');
     });
 });
+
